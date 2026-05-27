0459369a539e7c6c6f190e70d43aff2b425facec
sachin-zaggle <sachin.kaushik@zaggle.in>
2026-05-27 08:00:40 +0530

    add send email feature

---
+++ src/data/emailConfig.js
@@ -0,0 +1,13 @@
+export const emailConfig = [
+  { plantName: 'CDU', processEmail: 'cdu@12345gmail.com', rotaryMail: 'aayush@gmail.com', staticMail: 'vikhyat@gmail.com', inspectionMail: '', electricalMail: '' },
+  { plantName: 'MSB', processEmail: 'msb@123gmail.com', rotaryMail: 'shivaji@bharatpetroleum.in', staticMail: 'sanjay@gmail.com', inspectionMail: '', electricalMail: '' },
+  { plantName: 'DCU', processEmail: 'dcu@bharatpetroleum.in', rotaryMail: 'shivaji@bharatpetroleum.in', staticMail: 'Sandeep@gmail.com', inspectionMail: '', electricalMail: '' },
+  { plantName: 'HCU', processEmail: 'hcu@bharatpetroleum.in', rotaryMail: 'manojgupta@gmail.com', staticMail: 'Shashank@gmail.com', inspectionMail: '', electricalMail: '' },
+  { plantName: 'HGU', processEmail: 'hgu@bharat.in', rotaryMail: 'manojgupta@gmail.com', staticMail: 'Shubham@gmail.com', inspectionMail: '', electricalMail: '' },
+  { plantName: 'KHDS', processEmail: 'khds@gmail.com', rotaryMail: 'manojgupta@gmail.com', staticMail: 'Anuj@gmail.com', inspectionMail: '', electricalMail: '' },
+  { plantName: 'SRU', processEmail: 'SRU@BHARATPERROLEUM.COM', rotaryMail: 'ayushgupta@gmail.com', staticMail: 'Anuj@gmail.com', inspectionMail: '', electricalMail: '' },
+  { plantName: 'UTL', processEmail: 'jk@gmail.com', rotaryMail: 'viraaj@gmail.com', staticMail: 'viraj@gmail.com', inspectionMail: '', electricalMail: '' },
+  { plantName: 'CPP', processEmail: 'CPP@GMAIL.COM', rotaryMail: 'ashish@gmail.com', staticMail: 'jayant@gmail.com', inspectionMail: '', electricalMail: '' },
+  { plantName: 'OFF', processEmail: 'OFFSITE@GMAIL.COM', rotaryMail: 'Saurabh@gmail.com', staticMail: 'Pavan@gmail.com', inspectionMail: '', electricalMail: '' },
+  { plantName: 'BDT', processEmail: 'bdt@gmail.com', rotaryMail: 'mohit@gmail.com', staticMail: 'opsaini@gmail.com', inspectionMail: '', electricalMail: '' }
+];
